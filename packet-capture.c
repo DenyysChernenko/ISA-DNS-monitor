9fb7a3fe91b4c303c0ca7c79f0b6f3752239fdbc
DenyysChernenko <xchern08@stud.fit.vutbr.cz>
2024-10-11 18:44:55 +0200

    feat(packet-capture.c) - added verbose output logic (need to be fixed, some issues with domain names)

--- packet-capture.c
+++ packet-capture.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <time.h>
 #include <pcap.h>
 #include <unistd.h>
 #include "packet-capture.h"
@@ -23,6 +27,15 @@ int validate_interface(const char *interface) {
         }
     }
 
+    // Supportive print for seeing all valid interfaces
+    if (!interface_exist_flag) {
+        printf("The specified interface '%s' is not valid. Here are the available interfaces:\n", interface);
+        for (pcap_if_t *dev = alldevs; dev != NULL; dev = dev->next) {
+            printf(" - %s\n", dev->name);
+        }
+    }
+
+
     pcap_freealldevs(alldevs);
     return interface_exist_flag;
 }
@@ -36,27 +49,421 @@ int validate_pcap_file(const char *pcap_file) {
     return 1;
 }
 
+void print_dns_header(dns_header *header) {
+    printf("DNS Header Information:\n");
+    printf("Identifier: 0x%04X\n", header->id);
+    
+    printf("Flags:\n");
+    printf("  QR: %d\n", (header->flags & 0x8000) >> 15);
+    printf("  OPCODE: %d\n", (header->flags & 0x7800) >> 11);
+    printf("  AA: %d\n", (header->flags & 0x0400) >> 10);
+    printf("  TC: %d\n", (header->flags & 0x0200) >> 9);
+    printf("  RD: %d\n", (header->flags & 0x0100) >> 8);
+    printf("  RA: %d\n", (header->flags & 0x0080) >> 7);
+    printf("  Z: %d\n", (header->flags & 0x0070) >> 4);
+    printf("  AD: %d\n", (header->flags & 0x0020) >> 5);
+    printf("  CD: %d\n", (header->flags & 0x0010) >> 4);
+    printf("  RCODE: %d\n", header->flags & 0x000F);
+
+    printf("Questions Count: %d\n", header->q_count);
+    printf("Answer Count: %d\n", header->an_count);
+    printf("Authority Count: %d\n", header->ns_count);
+    printf("Additional Count: %d\n", header->ar_count);
+}
+
+void parse_dns_name(const u_char **reader, const u_char *packet, char *buffer, int *len) {
+    int jumped = 0; 
+    int offset;    
+    int pos = 0;    
+    const u_char *orig_reader = *reader; 
+
+    while (**reader != 0) {
+        if ((**reader & 0xC0) == 0xC0) {
+            offset = ntohs(*(uint16_t *)*reader) & 0x3FFF;  
+            *reader += 2;  
+
+            if (!jumped) { 
+                orig_reader = *reader;  
+            }
+            *reader = packet + offset; 
+            jumped = 1;  
+        } else {
+            int label_length = **reader; 
+            (*reader)++;  
+
+            strncpy(buffer + pos, (char *)(*reader), label_length);  
+            pos += label_length;
+            buffer[pos++] = '.'; 
+            *reader += label_length;  
+        }
+    }
+
+    buffer[pos - 1] = '\0';  
+    if (jumped) {
+        *reader = orig_reader;  
+    } else {
+        (*reader)++;  
+    }
+
+    *len = pos; 
+}
+
+
+
+void support_resource_record_parser(const u_char **reader, resource_record *records, int count, const u_char *packet) {
+    for (int i = 0; i < count; i++) {
+        records[i].name = (char *)malloc(256);
+
+        if (records[i].name == NULL) {
+            fprintf(stderr, "Failed to allocate memory for resource record\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int name_length = 0;
+        parse_dns_name(reader, packet, records[i].name, &name_length);
+
+        printf("Parsed domain name in Answer: %s\n", records[i].name);
+        records[i].type = ntohs(*(uint16_t *)(*reader));
+        (*reader) += 2;
+        records[i].a_class = ntohs(*(uint16_t *)(*reader));
+        (*reader) += 2;
+        records[i].ttl = ntohl(*(uint32_t *)(*reader));
+        (*reader) += 4;
+        records[i].rdlength = ntohs(*(uint16_t *)(*reader));
+        (*reader) += 2;
+
+        if (records[i].rdlength <= 0) {
+            fprintf(stderr, "Invalid data length, cannot be less or equal than zero\n");
+            exit(EXIT_FAILURE);
+        }
+
+        records[i].rdata = (char *)malloc(records[i].rdlength + 1);  
+        if (records[i].rdata == NULL) {
+            fprintf(stderr, "Failed to allocate memory for rdata for resource record\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if (records[i].type == 1) {  
+            if (records[i].rdlength == 4) {  
+                struct in_addr addr;
+                memcpy(&addr, *reader, sizeof(struct in_addr));  
+                records[i].rdata = (char *)malloc(INET_ADDRSTRLEN);  
+                inet_ntop(AF_INET, &addr, records[i].rdata, INET_ADDRSTRLEN);  
+            } else {
+                fprintf(stderr, "Invalid rdlength for A record: expected 4, got %d\n", records[i].rdlength);
+                exit(EXIT_FAILURE);
+            }
+        } else if (records[i].type == 28) {  
+            if (records[i].rdlength == 16) {  
+                struct in6_addr addr;
+                memcpy(&addr, *reader, sizeof(struct in6_addr));  
+                records[i].rdata = (char *)malloc(INET6_ADDRSTRLEN);  
+                inet_ntop(AF_INET6, &addr, records[i].rdata, INET6_ADDRSTRLEN);  
+            } else {
+                fprintf(stderr, "Invalid rdlength for AAAA record: expected 16, got %d\n", records[i].rdlength);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            records[i].rdata = (char *)malloc(records[i].rdlength + 1);  
+            memcpy(records[i].rdata, *reader, records[i].rdlength);
+            records[i].rdata[records[i].rdlength] = '\0';  
+        }
+
+        (*reader) += records[i].rdlength;  
+    }
+}
+
+void support_dns_packet_parser(const u_char *packet, dns_packet *dns) {
+    const u_char *dns_start = packet + 14 + sizeof(struct ip) + sizeof(struct udphdr);
+
+    // Parse DNS header with all flags, id and counts
+    dns->header = *(dns_header *)dns_start;
+    dns->header.id = ntohs(dns->header.id);
+    dns->header.flags = ntohs(dns->header.flags);
+    dns->header.q_count = ntohs(dns->header.q_count);
+    dns->header.an_count = ntohs(dns->header.an_count);
+    dns->header.ns_count = ntohs(dns->header.ns_count);
+    dns->header.ar_count = ntohs(dns->header.ar_count);
+
+    // print_dns_header(&dns->header);
+
+    const u_char *reader = dns_start + sizeof(dns_header);
+
+    // Parse DNS Question 
+    dns->questions = (question *)malloc(sizeof(question) * dns->header.q_count);
+
+    if(dns->questions == NULL) {
+        fprintf(stderr, "Failed to allocate data for DNS Questions\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    for (int i = 0; i < dns->header.q_count; i++) {
+        dns->questions[i].qname = (char *)malloc(256);
+
+        if(dns->questions[i].qname == NULL) {
+            fprintf(stderr, "Failde to allocate memory for dns question qname\n");
+            exit(EXIT_FAILURE);
+        }
+
+
+        int pos = 0;
+        while (*reader != 0) {
+             int label_length = *reader;
+             reader++; 
+             strncpy(dns->questions[i].qname + pos, (char *)reader, label_length);
+             pos += label_length;
+             dns->questions[i].qname[pos++] = '.';
+             reader += label_length; 
+         }
+          dns->questions[i].qname[pos - 1] = '\0'; 
+          reader++; 
+
+          dns->questions[i].qtype = ntohs(*(uint16_t *)reader);
+          reader+=2;
+          dns->questions[i].qclass = ntohs(*(uint16_t *)reader);
+          reader+=2;
+    }
+
+    // Parse Answer Section
+    if(dns->header.an_count > 0) {
+        dns->answers = (resource_record *)malloc(sizeof(resource_record) * dns->header.an_count);
+        if(dns->answers == NULL) {
+            fprintf(stderr, "Failed to allocate memory for dns answers\n");
+            exit(EXIT_FAILURE);
+        }
+        support_resource_record_parser(&reader, dns->answers, dns->header.an_count, packet);
+    }
+
+    // Parse Authority Section
+    if(dns->header.ns_count > 0) { 
+        dns->authorities = (resource_record *)malloc(sizeof(resource_record) * dns->header.ns_count);
+        if(dns->authorities == NULL) {
+            fprintf(stderr, "Failed to allocate memory for dns authorities\n");
+            exit(EXIT_FAILURE);
+        }
+        support_resource_record_parser(&reader, dns->authorities, dns->header.ns_count, packet);
+    }
+
+    // Parse Additional Section
+    if(dns->header.ar_count > 0) {
+        dns->additionals = (resource_record *)malloc(sizeof(resource_record) * dns->header.ar_count);
+         if(dns->additionals == NULL) {
+            fprintf(stderr, "Failed to allocate memory for dns additionals\n");
+            exit(EXIT_FAILURE);
+        }
+        support_resource_record_parser(&reader, dns->additionals, dns->header.ar_count, packet);
+    }
+
+
+
+}
+
+void verbose_packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
+    struct ip *ip_header;
+    struct udphdr *udp_header;
+    dns_packet dns;
+
+    // Take IP Header - 14 bytes after Ethernet header
+    ip_header = (struct ip *)(packet + 14);
+
+    // If IP protocol is not UDP -> skip (Protocol: UDP(17))
+    if (ip_header->ip_p != IPPROTO_UDP) {
+        return; 
+    }
+
+    // Extract UDP header 
+    udp_header = (struct udphdr *)(packet + 14 + ip_header->ip_hl * 4);
+    
+    
+    // Use support function to make comfort output 
+    support_dns_packet_parser(packet, &dns);
+
+    // Prepare timestamp for output
+    char time_str[64];
+    struct tm *ltime;
+    time_t local_tv_sec = pkthdr->ts.tv_sec;
+    ltime = localtime(&local_tv_sec);
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", ltime);
+
+    // Prepare for containing src_ip and dst_ip
+    char src_ip[INET_ADDRSTRLEN];
+    char dst_ip[INET_ADDRSTRLEN];
+
+    // Convert IP src and IP dst from binary to string
+    inet_ntop(AF_INET, &(ip_header->ip_src), src_ip, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(ip_header->ip_dst), dst_ip, INET_ADDRSTRLEN);
+
+    // Get source and destination port from UDP header
+    uint16_t src_port = ntohs(udp_header->uh_sport);
+    uint16_t dst_port = ntohs(udp_header->uh_dport);
+
+    // DNS Header flags
+    uint16_t flags = dns.header.flags;
+    uint8_t qr;
+    if ((flags & 0x8000) != 0) { 
+        qr = 1; 
+    } else {
+        qr = 0;
+    }
+    uint8_t opcode = (flags >> 11) & 0xF;
+    uint8_t aa = (flags >> 10) & 0x1;
+    uint8_t tc = (flags >> 9) & 0x1;
+    uint8_t rd = (flags >> 8) & 0x1;
+    uint8_t ra = (flags >> 7) & 0x1;
+    uint8_t ad = (flags >> 5) & 0x1;
+    uint8_t cd = (flags >> 4) & 0x1;
+    uint8_t rcode = flags & 0xF;
+
+    printf("Timestamp: %s\n", time_str);
+    printf("SrcIP: %s\n", src_ip);
+    printf("DstIP: %s\n", dst_ip);
+    printf("SrcPort: UDP/%d\n", src_port);
+    printf("DstPort: UDP/%d\n", dst_port);
+    printf("Identifier: 0x%04X\n", dns.header.id);
+    printf("Flags: QR=%d, OPCODE=%d, AA=%d, TC=%d, RD=%d, RA=%d, AD=%d, CD=%d, RCODE=%d\n", 
+           qr, opcode, aa, tc, rd, ra, ad, cd, rcode);
+
+
+    printf("\n[Question Section]\n");
+    for (int i = 0; i < dns.header.q_count; i++) {
+        printf("%s. IN %s\n", dns.questions[i].qname, 
+               (dns.questions[i].qtype == 1) ? "A" : 
+               (dns.questions[i].qtype == 28) ? "AAAA" : "UNKNOWN"); 
+    }
+
+
+    if (dns.header.an_count > 0) {
+        printf("\n[Answer Section]\n");
+        for (int i = 0; i < dns.header.an_count; i++) {
+            printf("%s. %d IN %s %s\n", dns.answers[i].name, dns.answers[i].ttl, 
+                (dns.answers[i].type == 1) ? "A" : (dns.answers[i].type == 28) ? "AAAA" : "UNKNOWN", dns.answers[i].rdata);
+        }
+    }
+
+    if (dns.header.ns_count > 0) {
+        printf("\n[Authority Section]\n");
+        for (int i = 0; i < dns.header.ns_count; i++) {
+            printf("%s. %d IN NS %s\n", dns.authorities[i].name, dns.authorities[i].ttl, dns.authorities[i].rdata);
+        }
+    }
+
+    if (dns.header.ar_count > 0) {
+        printf("\n[Additional Section]\n");
+        for (int i = 0; i < dns.header.ar_count; i++) {
+            printf("%s. %d IN %s %s\n", dns.additionals[i].name, dns.additionals[i].ttl, 
+                   (dns.additionals[i].type == 1) ? "A" : "UNKNOWN", dns.additionals[i].rdata);
+        }
+    }
+
+    
+
+
+    printf("\n");
+
+}
+
+
+
+void non_verbose_packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
+   struct ip *ip_header;
+   dns_header *dns;
+
+
+   // Take IP Header - 14 bytes after Ethernet header
+   ip_header = (struct ip *)(packet + 14);
+
+    // If ip protocol is not UDP -> skip (Protocol: UDP(17))
+    if (ip_header->ip_p != IPPROTO_UDP) {
+        return; 
+    }
+
+   // Take DNS Header, 14 (length of Ethernet header) + IP Header + + UDP headder (fixed size -> 8 bytes) 
+   dns = (dns_header *)(packet + 14 + (ip_header->ip_hl * 4) + sizeof(struct udphdr));
+
+   // Prepare timestamp for output
+   char time_str[64];
+   struct tm *ltime;
+   // Get the raw timestamp from packet header
+   time_t local_tv_sec = pkthdr->ts.tv_sec;
+   ltime = localtime(&local_tv_sec);
+   strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", ltime);
+
+   // Prepare for containing src_ip and dst_ip
+   char src_ip[INET_ADDRSTRLEN];
+   char dst_ip[INET_ADDRSTRLEN];
+
+   // Convert ip src and ip dst from binary to string
+   inet_ntop(AF_INET, &(ip_header->ip_src), src_ip, INET_ADDRSTRLEN);
+   inet_ntop(AF_INET, &(ip_header->ip_dst), dst_ip, INET_ADDRSTRLEN);
+
+   // Convert from network byte order to host byte order
+   uint16_t flags = ntohs(dns->flags);
+
+
+   char query_response;
+    if (flags & 0x8000) { 
+        query_response = 'R'; 
+    } else {
+        query_response = 'Q'; 
+    }
+
+   uint16_t question_count = ntohs(dns->q_count);
+   uint16_t answer_count = ntohs(dns->an_count);
+   uint16_t authority_count = ntohs(dns->ns_count);
+   uint16_t additional_count = ntohs(dns->ar_count);
+
+   printf("%s %s -> %s (", time_str, src_ip, dst_ip);
+   printf("%c %d/%d/%d/%d)\n",
+           query_response,          
+           question_count,          
+           answer_count,            
+           authority_count,         
+           additional_count);      
+}
+
+
+pcap_t *setup_filter(pcap_t *handle, char *filter_exp, bpf_u_int32 net) {
+    struct bpf_program fp;
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse the filter exp: %s\n", filter_exp);
+        return NULL;
+    }
+
+    if (pcap_setfilter(handle, &fp) == -1) {
+        fprintf(stderr, "Couldn't set filter exp: %s\n", filter_exp);
+        return NULL;
+    }
+
+    return handle;
+}
+
 
 void start_packet_capture(Arguments *arguments) {
 
-    // Debuggin prints
-    printf("Starting packet capture with the following settings:\n");
-    if(strlen(arguments->interface) == 0) {
-        printf("Interface : Not provided\n");
-    } else {
-        printf("Interface: %s\n", arguments->interface);
-    }
-    printf("Verbose: %d\n", arguments->verbose);
-    printf("PCAP File: %s\n", arguments->pcap_file ? arguments->pcap_file : "Not provided");
-    printf("Domain File: %s\n", arguments->domain_file ? arguments->domain_file : "Not provided");
-    printf("Translation File: %s\n", arguments->translation_file ? arguments->translation_file : "Not provided");
-    printf("\n");
-
-
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char filter_exp[] = "udp port 53";
+    bpf_u_int32 net = 0;   
 
     if (strlen(arguments->interface) > 0) {
         if(validate_interface(arguments->interface)) {
-            printf("Interface: %s is valid\n", arguments->interface);
+            handle = pcap_open_live(arguments->interface, BUFSIZ, 1, 1000, errbuf);
+            if(handle == NULL) {
+                fprintf(stderr, "Failed to open interface %s: %s\n", arguments->interface, errbuf);
+                return;
+            }
+
+            if(setup_filter(handle, filter_exp, net) == NULL) {
+                pcap_close(handle);
+                return;
+            }
+            if(arguments->verbose == 1)  {
+                pcap_loop(handle, 0, verbose_packet_handler, NULL);
+            } else {
+                pcap_loop(handle, 0, non_verbose_packet_handler, NULL);
+            }
+            pcap_close(handle);
+
         } else {
             fprintf(stderr, "Interface '%s' doesnt exist\n", arguments->interface);
             return;
@@ -65,11 +472,33 @@ void start_packet_capture(Arguments *arguments) {
 
     if(arguments->pcap_file) {
         if(validate_pcap_file(arguments->pcap_file)) {
-            printf("PCAP File %s is valid\n", arguments->pcap_file);
+            handle = pcap_open_offline(arguments->pcap_file, errbuf);
+
+            if(handle == NULL) {
+                fprintf(stderr, "Coudldn't open the PCAP File %s: %s\n", arguments->pcap_file, errbuf);
+                return;
+            }
+
+            if(setup_filter(handle, filter_exp, net) == NULL) {
+                pcap_close(handle);
+                return;
+            }
+
+            printf("-------START CAPTURING PCAP FILES-------\n");
+            if(arguments->verbose == 1) {
+                pcap_loop(handle, 0, verbose_packet_handler, NULL);
+            } else {
+                pcap_loop(handle, 0, non_verbose_packet_handler, NULL);
+            }
+                pcap_close(handle);
+
         } else {
             fprintf(stderr, "PCAP File is not valid\n");
             return;
         }
     }
+
+
+
 
 }
--- packet-capture.h
+++ packet-capture.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pcap.h>
 #include <stdint.h>
 #include "arguments-parse.h"
 
@@ -11,23 +12,22 @@ typedef struct DNS_HEADER  {
     uint16_t id; // Unique identifier
 
     // Flags (detailed)
-    uint16_t qr : 1;     // One bit specified whether this is a query or response
-    uint16_t opcode : 4; // Specifies type of query (standard query, inverse query etc.)
-    uint16_t aa : 1;     // Authoritative Answer (valid in responses)
-    uint16_t tc : 1;     // TrunCation  (if message was truncated due to length)
-    uint16_t rd : 1;     // Recursion Desired (if set it directs name server to pursue the query recursively)
-    uint16_t ra : 1;     // Recursion Available 
-    uint16_t z  : 3;     // Reserved for future (must be zero in all queries/responses)
-    uint16_t rcode: 4;   // part of response (0 - no error, more than 0 some issues)
+    // uint16_t qr : 1;     // One bit specified whether this is a query or response    
+    // uint16_t opcode : 4; // Specifies type of query (standard query, inverse query etc.)
+    // uint16_t aa : 1;     // Authoritative Answer (valid in responses)
+    // uint16_t tc : 1;     // TrunCation  (if message was truncated due to length)
+    // uint16_t rd : 1;     // Recursion Desired (if set it directs name server to pursue the query recursively)
+    // uint16_t ra : 1;     // Recursion Available 
+    // uint16_t z  : 3;     // Reserved for future (must be zero in all queries/responses)
+    // uint16_t rcode: 4;   // part of response (0 - no error, more than 0 some issues)
+
+    uint16_t flags; 
 
     uint16_t q_count;  // Number of entries in the question section (Questions)
     uint16_t an_count; // Number of resource records in the answer section (Answers)
     uint16_t ns_count; // Number of name servers (Authority)
     uint16_t ar_count; // Number of Additional resource (Additional)
 
-    // Optional only in responses
-    uint16_t ad     : 1;    // Authenticated Data 
-    uint16_t cd     : 1;    // Checking Disabled 
 } dns_header;
 
 
@@ -64,7 +64,6 @@ typedef struct DNS_PACKET {
 // Starting point to capture DNS packets
 void start_packet_capture(Arguments *arguments);
 
-
 /**
  * @brief Checks if a given network interface exists on the system
  *
@@ -73,14 +72,90 @@ void start_packet_capture(Arguments *arguments);
  */
 int validate_interface(const char *interface);
 
-
 /**
- * @brief Validates if a PCAP file is accessible for reading (So it can sniff for DNS packets in it).
+ * @brief Validates if a PCAP file is accessible for reading (So it can sniff for DNS packets in it)
  *
- * @param pcap_file The PCAP file to validate.
- * @return 1 if the file is accessible, 0 otherwise.
+ * @param pcap_file The PCAP file to validate
+ * @return 1 if the file is accessible, 0 otherwise
  */
 int validate_pcap_file(const char *pcap_file);
 
+/**
+ * @brief Prints the details of a DNS header (with debugging purposes)
+ *
+ *
+ * @param header Pointer to a dns_header structure that contains the DNS header information
+ */
+void print_dns_header(dns_header *header);
+
+/**
+ * @brief Parses a DNS name from the DNS packet
+ *
+ * This function extracts a DNS name from the packet, handling both
+ * normal labels and compressed names using pointers.
+ * 
+ * @param reader current position in the packet being parsed
+ * @param packet DNS packet 
+ * @param buffer Buffer to store the parsed DNS name
+ * @param len length of the parsed DNS name
+ */
+void parse_dns_name(const u_char **reader, const u_char *packet, char *buffer, int *len);
+
+/**
+ * @brief Parses resource records from a DNS packet.
+ *
+ * This function reads resource records from a DNS packet and stores
+ * them in the provided array(Such as answers, authorities or additionals).
+ *
+ * @param reader current position in the packet being parsed
+ * @param records array to store the parsed resource records
+ * @param count number of resource records to parse
+ * @param packet original DNS packet data
+ */
+void support_resource_record_parser(const u_char **reader, resource_record *records, int count, const u_char *packet);
+
+/**
+ * @brief Parses a DNS packet and extracts its components
+ *
+ * This function takes a raw DNS packet, extracts and decodes its header
+ * question, answer, authority, and additional sections
+ * 
+ * @param packet raw DNS packet data
+ * @param dns  dns_packet structure where parsed data will be stored.
+ */
+void support_dns_packet_parser(const u_char *packet, dns_packet *dns);
+
+/**
+ * @brief Handles and processes incoming DNS packets in non verbose mode (not complex output)
+ *
+ *
+ * @param user_data Extra data you can pass to the handler (not used here)
+ * @param pkthdr  A pointer to the packet header with details about the captured packet
+ * @param packet A pointer to the actual packet data from the network
+ */
+void non_verbose_packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet);
+
+
+/**
+ * @brief Handles and processes incoming DNS packets in non verbose mode (complex output)
+ *
+ *
+ * @param user_data Extra data you can pass to the handler (not used here)
+ * @param pkthdr  A pointer to the packet header with details about the captured packet
+ * @param packet A pointer to the actual packet data from the network
+ */
+void verbose_packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet);
+
+
+/**
+ * @brief Support function for settuping filters (made for comfort using)
+ *
+ *
+ * @param handle A pointer to the pcap handle for the capture or file to apply the filter on
+ * @param filter_exp  A string containing the filter expression to be set
+ * @param net The network address associated with the capture device, set to 0 for offline capture
+ * @return A pointer to the pcap handle if the filter is set successfully; NULL if some error occurs
+ */
+pcap_t *setup_filter(pcap_t *handle, char *filter_exp, bpf_u_int32 net);
 
 #endif
